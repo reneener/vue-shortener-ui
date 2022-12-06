240f89b48834fd08dfaafdbdad228bf6ae905d93
reneener <jhjin2u@gmail.com>
2022-12-06 18:13:51 +0900

    vue 3 version

---
+++ src/components/Input.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <input type="text" v-model="newUrl">
+  <button>추가</button>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style >
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/dist/vuetify.min.css'
+import { md } from 'vuetify/lib/iconsets/md'
 
-createApp(App).use(router).use(store).use(router).mount('#app')
+loadFonts(md)
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
